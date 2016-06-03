9b9b7354db67ff57aed5a9185e5465b7ea9d750a
Ronny Mathew <ronnygeomatt@gmail.com>
2016-06-03 12:50:14 -0400

    Adding more actions and routes.

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello!!! You are in the polls section!")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are in the detail view for the question %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You are in the results view for the question %s" % question_id)
+
+
+def vote(request, question_id, choice_id):
+    return HttpResponse("You are voting for the question %s with choice %s" % (question_id, choice_id))
